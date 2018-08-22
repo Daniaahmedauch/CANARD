f6245e0c8c14ed584dae4b90747fbce1c2093a2e
Daniaahmedauch <39794400+Daniaahmedauch@users.noreply.github.com>
2018-08-22 13:35:12 +0200

    tableau.php

--- canard.php
+++ canard.php
@@ -13,119 +13,37 @@
 	<title> </title>
 </head>
 <body>
-  <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#"> LES AJOUTS DE CANARD </a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">  TOUT SUR LES CANARD </a></li>
-      <li><a href="#"> LE TABLEAU </a></li>
-      <li><a href="#">FACEBOOK </a></li>
-    </ul>
-  </div>
-</nav>
 
 
-      <table>
-        <thead>
-          <tr>
-              <th> OU </th>
-              <th>QUAND</th>
-              <th>COMMENTAIRES</th>
-          </tr>
-        </thead>
 
-        <tbody>
-          <tr>
-            <td>QUAND </td>
-            <td>Eclair</td>
-            <td>$0.87</td>
-          </tr>
-          <tr>
-            <td>COMMENTAIRES</td>
-            <td>Jellybean</td>
-            <td>$3.76</td>
-          </tr>
-          </tr>
-        </tbody>
-      </table>
-            
-
-<?php
-
-	   /*
-	   		Enregistrer le nouveau post
-	   */
-
-       $titre=$_POST['titre'];
-       $contenus=$_POST['contenus'];
-       $commentaires=$_POST['commentaires'];
-       
-
-         // on se connecte à mysql :
-
-         
-        try 
-       {  
-
-           $bdd = new PDO( 'mysql:host=localhost;dbname=canard;charset=utf8','dania','0000');
-         
-       }
-
-          // en cas d'erreur on affiche un message :
+    <center>  <h4>  AJOUTE TON CANARD</h4> </center>
 
         
-        catch (Exception $e)
-        {   
-           die('Erreur : ' . $e->getMessage());
-      
-        }
+        <table  class="striped" class="responsive-table">
+        
+            <tr>
+          
+              <th>ou</th>
+              <th>quand</th>
+              <th>commentaires</th>
 
-       $req = $bdd->prepare('
-       		INSERT INTO form ( titre,contenus,commentaires ) 
-       		VALUES( :titre, :contenus, :commentaires );
-       	');
+            </tr>
 
-       $req->execute(array(
-	    	':titre' => $titre,
-	    	':contenus' => $contenus,
-       		':commentaires' => $commentaires
-       		
-       ));
-
-
-
-
-
-   	   /*
-
-   	   		Afficher la page
-   	   	*/
-
-       $reponse = $bdd->query('SELECT * FROM form');
-       while($donnees=$reponse->fetch())
-
-	 {
-
-?>
-
-		<!--  affichage des données -->
-<?php
-			echo '<p> ' . $donnees ['titre'] .  ' </p> '; 
-			echo '<p>'  . $donnees['contenus'] .'</p>';
-			echo '<p>'  . $donnees['commentaires'] .'</p>';
-			
-      }
-
-?>
-
-<?php 
-
-      echo '<tr><td>'OU: . $donnees ['']
-
-?>
-	
+    <?php
+    
+    
+    $bdd = new PDO('mysql:host=localhost;dbname=canard;charset=utf8', 'dania','0000');
+    $reponse = $bdd->query("SELECT * FROM form");
+    while($donnees = $reponse -> fetch())
+    echo
+    '<tr>
+      <td>'.  $donnees['ou'].'</td>
+      <td>'.  $donnees['quand'].'</td>
+      <td>'.  $donnees['commentaires'].'</td>
+    </tr>';
+    
+    ?>
+</table> 
 
 
 </body>
